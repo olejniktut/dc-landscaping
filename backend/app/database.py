724fe830f989ea199c97c3c74664a842c84fef84
EvgeniiOliinyk <42338010+olejniktut@users.noreply.github.com>
2025-12-14 15:29:51 -0600

    Update database.py
    database fv3

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://root:EtuOneRIKNNMvOOgbVNmffSpChGZyAfG@mainline.proxy.rlwy.net:13255/railway"
+DATABASE_URL = "mysql+pymysql://root:EtuOneRIKNNMvOOgbVNmffSpChGZyAfG@mysql.railway.internal:3306/railway"
 
 engine = create_engine(
     DATABASE_URL,
